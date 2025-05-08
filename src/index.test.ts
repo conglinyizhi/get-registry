aea57650d0784694467f945f5ca72236c9372b30
conglinyizhi <cslbyivi@outlook.com>
2025-05-08 22:09:45 +0800

    test(index): Add unit test for the get function

---
+++ src/index.test.ts
@@ -0,0 +1,10 @@
+import get from "./index.js";
+import { describe, it, expect } from "vitest";
+
+describe("get", () => {
+  it("should return the registry", async () => {
+    const registry = await get();
+    console.log(registry);
+    expect(registry).toBeDefined();
+  });
+});
